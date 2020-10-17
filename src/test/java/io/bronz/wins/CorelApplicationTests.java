c9a1a58dc0ad1a49cd169da51ca8fba030778690
Szymon Drawski <szymondrawski@MacBook-Pro.local>
2020-10-17 12:15:34 +0200

    Initial commit

---
+++ src/test/java/io/bronz/wins/CorelApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bronz.wins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
