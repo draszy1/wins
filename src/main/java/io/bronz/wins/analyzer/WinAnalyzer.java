01a3196a709cd653e4a23583f844e918d030f551
draszy1 <draszy1@gmail.com>
2020-10-17 13:57:10 +0200

    Same wins

--- src/main/java/io/bronz/wins/analyzer/WinAnalyzer.java
+++ src/main/java/io/bronz/wins/analyzer/WinAnalyzer.java
@@ -2,24 +2,45 @@ package io.bronz.wins.analyzer;
 
 import io.bronz.wins.data.HistoricalResult;
 import io.bronz.wins.file.ResultFileReader;
+import io.bronz.wins.out.CorelationPresenter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class WinAnalyzer {
 
+    private static final int ONE_WIN = 1;
     private final ResultFileReader resultFileReader;
+    private final CorelationPresenter corelationPresenter;
 
     public void analyzeResults() {
-        List<HistoricalResult> historicalResults = resultFileReader.getHistoricalResults();
+        List<HistoricalResult> historicalResults = getHistoricalResults();
 
-        for (HistoricalResult historicalResult : historicalResults) {
-            log.info("Date: {}, Result: {}", historicalResult.getDate(), historicalResult.getResult());
-        }
+        sameWins(historicalResults);
+    }
+
+    private void sameWins(List<HistoricalResult> historicalResults) {
+        Map<String, List<HistoricalResult>> theSameWins = historicalResults.stream()
+                .collect(groupingBy(HistoricalResult::getResult))
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().size() > ONE_WIN)
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        corelationPresenter.presentSameWins(theSameWins);
+    }
+
+
+    private List<HistoricalResult> getHistoricalResults() {
+        return resultFileReader.getHistoricalResults();
     }
 }
--- src/main/java/io/bronz/wins/out/CorelationPresenter.java
+++ src/main/java/io/bronz/wins/out/CorelationPresenter.java
@@ -1,7 +1,34 @@
 package io.bronz.wins.out;
 
+import io.bronz.wins.data.HistoricalResult;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class CorelationPresenter {
+    public void presentSameWins(Map<String, List<HistoricalResult>> theSameWins) {
+
+        if (theSameWins.isEmpty()) {
+            System.out.println("THERE WAS NEVER THE SAME SET OF NUMBERS DRAWN.");
+        }
+
+        System.out.println("THE SAME WINS DURING HISTORY");
+        System.out.println("==============================");
+
+        for (var win : theSameWins.entrySet()) {
+            System.out.println("Numbers: " + win.getKey());
+
+            presentWinDates(win);
+        }
+    }
+
+    private void presentWinDates(Map.Entry<String, List<HistoricalResult>> win) {
+        for (var winDetails : win.getValue()) {
+            System.out.println(winDetails.getDate());
+        }
+
+        System.out.println("---------------------");
+    }
 }
